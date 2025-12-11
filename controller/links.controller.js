2c71c69c922a6ec3bb38e4cb0f59f60e5bd4c594
Ini Rizky <85105054+inirizky@users.noreply.github.com>
2025-12-11 09:04:19 +0700

    feat: implement regex to slug, change error response

--- controller/links.controller.js
+++ controller/links.controller.js
@@ -10,29 +10,35 @@ export const createLinks = async (req, res) => {
     return res.status(400).json({ error: "URL are required." });
   }
 
+  const transformSlug = slug.replace(/[^a-zA-Z0-9]/g, "");
+
   try {
     await prisma.links.create({
       data: {
         slug: slug
-          ? slug
+          ? transformSlug
           : (
-              Date.now().toString(5) +
-              Math.random().toString(36).substring(2, 8)
-            ).slice(-5),
+            Date.now().toString(5) +
+            Math.random().toString(36).substring(2, 8)
+          ).slice(-5),
         url,
         userId: req?.user.id,
       },
     });
-
     res.status(201).json({ message: "Create shortlink successfully" });
   } catch (error) {
-    console.log(error);
-
-    res.status(400).json({ error: "Internal server error" });
+    if (error.code === "P2002") {
+      return res.status(400).json({ message: "Slug already exists" });
+    }
+    res.status(400).json({ message: "Internal server error" });
   }
 };
 export const readLinkBySlug = async (req, res) => {
   const { slug } = req.params;
+
+  if (!slug) {
+    return res.status(400).json({ error: "Slug are required." });
+  }
   try {
     const link = await prisma.links.findFirstOrThrow({
       where: {
@@ -40,25 +46,21 @@ export const readLinkBySlug = async (req, res) => {
         userId: req.user.id,
       },
     });
-
     res.status(200).json({ message: "Success", status: 200, data: link });
   } catch (error) {
-    console.log(error);
     res.status(400).json({ message: "Error", error: error, status: 400 });
   }
 };
 export const readLinks = async (req, res) => {
+
   try {
     const links = await prisma.links.findMany({
       where: {
         userId: req.user.id,
       },
     });
-    console.log(links);
-
     res.status(200).json({ message: "Success", data: links });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: "Error", error: error });
   }
 };
@@ -66,10 +68,15 @@ export const editLink = async (req, res) => {
   const { id } = req.params;
   const { url, slug } = req.body;
 
+  if (!id) {
+    return res.status(400).json({ error: "ID are required." });
+  }
+
   if (!url) {
     return res.status(400).json({ error: "URL are required." });
   }
 
+  const transformSlug = slug.replace(/[^a-zA-Z0-9]/g, "");
   try {
     await prisma.links.update({
       where: {
@@ -77,15 +84,17 @@ export const editLink = async (req, res) => {
         userId: req.user.id,
       },
       data: {
-        slug,
+        slug: transformSlug,
         url,
       },
     });
 
     res.status(200).json({ message: "Edit succesfully" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Internal server error" });
+    if (error.code === "P2002") {
+      return res.status(400).json({ message: "Slug already exists" });
+    }
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 export const deleteLink = async (req, res) => {
@@ -103,7 +112,7 @@ export const deleteLink = async (req, res) => {
 
     res.status(200).json({ message: "Link has been deleted" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error", error: error });
+
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,7 +2,6 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
-import authenticateToken from "../middleware/auth.js";
 
 const prisma = new PrismaClient({});
 
